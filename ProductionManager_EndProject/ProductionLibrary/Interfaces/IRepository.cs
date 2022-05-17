5cb2e667311e17af65b88180f8408557e5e95617
Poniatowski Maximilian <Poniatowski-max@outlook.fr>
2022-05-17 16:03:56 +0200

    [UPD] Checkpoint-INTEC

---
+++ ProductionManager_EndProject/ProductionLibrary/Interfaces/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionLibrary.Interfaces
+{
+    interface IRepository<T>
+    { 
+        Task<IEnumerable<T>> ListAll();
+        IQueryable<T> GetAll();
+        Task<T> GetById<X>(X id);
+
+        //Create
+        Task<T> Create(T entity);
+
+        //Delete
+        Task<T> Delete(T entity);
+        Task<T> DeleteById(int id);
+
+        //Update
+        Task<T> Update(T entity);
+
+
+    }
+}
